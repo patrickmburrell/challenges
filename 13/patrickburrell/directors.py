784e5d5f07318ebca41660262474f475894c7651
Patrick Burrell <patrick.burrell@amway.com>
2020-07-25 10:28:13 -0400

    now more Pythonic!

--- 13/patrickburrell/directors.py
+++ 13/patrickburrell/directors.py
@@ -1,5 +1,5 @@
 import csv
-from collections import defaultdict, namedtuple
+from collections import defaultdict, namedtuple, Counter
 from pathlib import Path
 import itertools
 
@@ -15,62 +15,47 @@ def get_movies_by_director():
     """Extracts all movies from csv and stores them in a dictionary
     where keys are directors, and values is a list of movies (named tuples)"""
 
-    directors = {}
+    movies_by_director = defaultdict(list)
     folder = Path(__file__).parent
-    with open(folder / MOVIE_DATA, newline="") as csvfile:
-        reader = csv.reader(csvfile)
-        columns = reader.__next__()
-
-        director_index = columns.index("director_name")
-        title_index = columns.index("movie_title")
-        year_index = columns.index("title_year")
-        score_index = columns.index("imdb_score")
+    with open(folder / MOVIE_DATA) as csvfile:
+        reader = csv.DictReader(csvfile)
 
         for row in reader:
-            director = row[director_index]
-            movie = Movie(
-                title=row[title_index],
-                year=_try_int(row[year_index]),
-                score=float(row[score_index]),
-            )
+            try:
+                director = row["director_name"]
+                movie = Movie(
+                    title=row["movie_title"],
+                    year=int(row["title_year"]),
+                    score=float(row["imdb_score"]),
+                )
+            except ValueError:
+                continue
+            movies_by_director[director].append(movie)
 
-            if director not in directors:
-                directors[director] = {"movies": []}
-            if movie.year >= MIN_YEAR:
-                directors[director]["movies"].append(movie)
-
-    return directors
+    return movies_by_director
 
 
 def get_average_scores(directors):
     """Filter directors with < MIN_MOVIES and calculate averge score"""
 
-    directors = {k: v for k, v in directors.items() if len(v["movies"]) >= MIN_MOVIES}
+    directors = {k: v for k, v in directors.items() if len(v) >= MIN_MOVIES}
 
-    for director in directors:
-        directors[director]["mean_score"] = _calc_mean(directors[director]["movies"])
+    score_ranks = defaultdict(list)
+    for director, movies in directors.items():
+        score_ranks[director].append(_calc_mean(movies))
 
-    return directors
+    score_ranks = Counter(score_ranks).most_common(NUM_TOP_DIRECTORS)
+
+    return directors, score_ranks
 
 
 def _calc_mean(movies):
     """Helper method to calculate mean of list of Movie namedtuples"""
-    sum = 0.0
-    for movie in movies:
-        sum += movie.score
-    mean = sum / len(movies)
+    mean = round(sum(movie.score for movie in movies) / len(movies), 1)
     return mean
 
 
-def _try_int(s):
-    try:
-        i = int(s)
-    except ValueError:
-        i = 0  # the default value
-    return i
-
-
-def print_results(directors):
+def print_results(directors, score_ranks):
     """Print directors ordered by highest average rating. For each director
     print his/her movies also ordered by highest rated movie.
     See http://pybit.es/codechallenge13.html for example output"""
@@ -78,33 +63,25 @@ def print_results(directors):
     fmt_movie_entry = "{year}] {title:<50} {score}"
     sep_line = "-" * 60
 
-    ordered_directors = {
-        k: v
-        for k, v in sorted(
-            directors.items(), key=lambda item: item[1]["mean_score"], reverse=True
-        )
-    }
-
-    top20_directors = dict(
-        itertools.islice(ordered_directors.items(), NUM_TOP_DIRECTORS)
-    )
-
-    for i, (k, v) in enumerate(top20_directors.items()):
-        print(fmt_director_entry.format(counter=i + 1, director=k, avg=v["mean_score"]))
+    for i in range(len(score_ranks)):
+        director = score_ranks[i][0]
+        average = score_ranks[i][1][0]
+        print(fmt_director_entry.format(counter=i + 1, director=director, avg=average))
         print(sep_line)
 
-        movies = v["movies"]
-        ordered_movies = [
+        movies = directors[director]
+        sorted_movies = [
             m for m in sorted(movies, key=lambda item: item.score, reverse=True)
         ]
 
-        for movie in ordered_movies:
+        for movie in sorted_movies:
             print(
                 fmt_movie_entry.format(
                     year=movie.year, title=movie.title, score=movie.score
                 )
             )
-        print("\n")
+
+        print()
 
 
 def main():
@@ -112,8 +89,8 @@ def main():
     We wrote some tests based on our solution: test_directors.py"""
 
     directors = get_movies_by_director()
-    directors = get_average_scores(directors)
-    print_results(directors)
+    directors, score_ranks = get_average_scores(directors)
+    print_results(directors, score_ranks)
 
 
 if __name__ == "__main__":
